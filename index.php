fc5986d2a03ed396d8a868f81f49e1d807778dae
sshhdaniella <daniella.taylor92@gmail.com>
2017-03-05 18:17:41 +0000

    Added updated site files

--- index.php
+++ index.php
@@ -8,10 +8,26 @@
 </head>
 <body>
 	
+
+	<?php 
+
+		$greetings = [
+			0 => "You're a little early",
+			1 => "A little too eager",
+			2 => "Pleased to meet you",
+			3 => "Coming Soon"
+		];
+
+		$random = array_rand($greetings, 1);
+		$randomGreeting = strtoupper($greetings[$random]);
+
+
+	?>
+
 	<div class="main-wrapper">
 
 		<div class="content">
-			<h1>A little early</h1>
+			<h1><?php echo $randomGreeting; ?></h1>
 			<p>Feel free to say <a href="mailto:hello@daniellataylor.co.uk">hello</a></p>
 		</div>
 	</div>
