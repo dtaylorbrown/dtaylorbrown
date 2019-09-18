c2315d2c1250396242b7c051462dbc73d8b7ed75
Daniella Taylor <daniella.taylor92@gmail.com>
2019-09-18 17:34:48 +0100

    added theme and logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,20 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import styled, { ThemeProvider } from 'styled-components';
+
+import { theme } from '../styles/theme';
 
 import Header from "./header"
 import "./layout.css"
+
+const Wrapper = styled.div`
+  max-width: ${props => props.theme.sizes.large};
+  width: 100%;
+  margin: 0 auto;
+  background: ${props => props.theme.colors.uiBase};
+  color: ${props=> props.theme.colors.uiContrast};
+`;
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,24 +35,19 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+      <>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <ThemeProvider theme={theme}>
+          <Wrapper>
+            <main>{children}</main>
+            <footer>
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+          </Wrapper>
+        </ThemeProvider>
+      </>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,12 @@ import React from "react"
 import styled from 'styled-components';
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const H1 = styled.h1`
-  color: rebeccapurple;
 `;
 
 const PTag = styled.p`
-  color: rebeccapurple;
-`;
-
-const StyledWrap = styled.div`
-  max-width: 300px;
-  margin-bottom: 1.45rem;
 `;
 
 const IndexPage = () => (
@@ -24,9 +16,6 @@ const IndexPage = () => (
     <H1>Hi people</H1>
     <PTag>Welcome to your new Gatsby site.</PTag>
     <PTag>Now go build something great.</PTag>
-    <StyledWrap>
-      <Image />
-    </StyledWrap>
   </Layout>
 )
 
