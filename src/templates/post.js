acba191e9116128c692e3017f38d5c3ad37f6916
Daniella Taylor <daniella.taylor92@gmail.com>
2020-05-02 22:21:45 +0100

    :sparkles: - adding new gatsby files, connected to ghost content api

---
+++ src/templates/post.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+const Post = ({data: {post}}) => {
+    const title = post && post.edges[0].node.title;
+    const html = post && post.edges[0].node.html;
+    return(
+        <main style={{ textAlign: 'center', padding: '50px' }}>
+            <h1>{title}</h1>
+            <article style={{ maxWidth: '840px', margin: '0 auto' }} dangerouslySetInnerHTML={{__html: html}} />
+        </main>
+    )
+}
+
+export default Post;
+
+//query all ghost posts to get next & previous
+export const postQuery = graphql`
+    query($id: String!){
+        post: allGhostPost(filter: { ghostId: {eq: $id} }){
+            edges {
+                previous {
+                    slug
+                }
+                node {
+                    title
+                    feature_image
+                    slug
+                    html
+                }
+                next {
+                    slug
+                }
+            }
+        }
+    }
+`;
